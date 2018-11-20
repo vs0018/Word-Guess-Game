da9f1dab5e14ee3989a07f99bef2c77ce79a5a29
Valerie S <43964677+vs0018@users.noreply.github.com>
2018-11-20 16:24:45 -0500

    fix html ids, fix problem with space being pushed into array placeholders

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-// Available passwords
+// VARIABLES
 var passwords =
     [
         "abcde",
@@ -7,89 +7,68 @@ var passwords =
         "pqrst",
     ];
 
-// Maximum number of tries to guess
 var maxTries = 10;            
-
-// Stores the input letters
 var guessedLetters = [];
-
-// Index of the current word in the array
 var currentWordIndex;
-
-// Fill in the blank
-var guessingWord = [];
-
-// How many tries the player has left
+var currentPass = [];
 var remainingGuesses = 0;
 
-// Flag to tell if the game has started
 var gameStarted = false;
-
-// Flag for 'press any key to try again'
 var hasFinished = false;
 
-// How many wins has the player has achieved
 var wins = 0;                   
 
-// Reset our game-level variables
+//GAME LOGIC
+
 function resetGame() {
     remainingGuesses = maxTries;
     gameStarted = false;
 
-    // Use Math.floor to round the random number down to the nearest whole.
     currentWordIndex = Math.floor(Math.random() * (passwords.length));
 
-    // Clear out arrays
     guessedLetters = [];
-    guessingWord = [];
+    currentPass = [];
 
-    // Make sure the hangman image is cleared
-    // document.getElementById("hangmanImage").src = "";
+    // want to add typewriter function to make instructions type across page
 
-    // Build the guessing word and clear it out
     for (var i = 0; i < passwords[currentWordIndex].length; i++) {
-        guessingWord.push("_");
+        currentPass.push("_");
     }
 
+
     // Hide game over and win images/text
-    document.getElementById("pressKeyTryAgain").style.cssText= "display: none";
+    document.getElementById("pressKey").style.cssText= "display: none";
     document.getElementById("gameover").style.cssText = "display: none";
     document.getElementById("youwin").style.cssText = "display: none";
+    document.getElementById("startGame").style.cssText = "display: block";
 
-    // Show display
-    updateDisplay();
+    updatePage();
 };
 
-//  Updates the display on the HTML Page
-function updateDisplay() {
+function updatePage() {
 
     document.getElementById("totalWins").innerText = wins;
-    document.getElementById("currentWord").innerText = "";
-    for (var i = 0; i < guessingWord.length; i++) {
-        document.getElementById("currentWord").innerText += guessingWord[i];
+    document.getElementById("yourPass").innerText = "";
+    for (var i = 0; i < currentPass.length; i++) {
+        document.getElementById("yourPass").innerText += currentPass[i];
     }
     document.getElementById("remainingGuesses").innerText = remainingGuesses;
     document.getElementById("guessedLetters").innerText = guessedLetters;
 
-// Determines if user is out of guesses and displays appropriate message
     if(remainingGuesses <= 0) {
         document.getElementById("gameover").style.cssText = "display: block";
-        document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
+        document.getElementById("pressKey").style.cssText = "display:block";
         hasFinished = true;
     }
 };
 
-// Updates the image depending on how many guesses
-// function updateHangmanImage() {
-//     document.getElementById("hangmanImage").src = "assets/images/" + (maxTries - remainingGuesses) + ".png";
-// };
 
 document.onkeydown = function(event) {
-    // If we finished a game, dump one keystroke and reset.
     if(hasFinished) {
         resetGame();
         hasFinished = false;
     } else {
+        document.getElementById("startGame").style.cssText = "display: none";
         makeGuess(event.key.toLowerCase());
     }
 };
@@ -100,46 +79,39 @@ function makeGuess(letter) {
             gameStarted = true;
         }
 
-        // Make sure we didn't use this letter yet
         if (guessedLetters.indexOf(letter) === -1) {
             guessedLetters.push(letter);
             evaluateGuess(letter);
         }
     }
     
-    updateDisplay();
+    updatePage();
     checkWin();
 };
 
-// This function takes a letter and finds all instances of 
-// appearance in the string and replaces them in the guess word.
 function evaluateGuess(letter) {
-    // Array to store positions of letters in string
+
     var positions = [];
 
-    // Loop through word finding all instances of guessed letter, store the indicies in an array.
     for (var i = 0; i < passwords[currentWordIndex].length; i++) {
         if(passwords[currentWordIndex][i] === letter) {
             positions.push(i);
         }
     }
 
-    // if there are no indicies, remove a guess and update the hangman image
     if (positions.length <= 0) {
         remainingGuesses--;
-        // updateHangmanImage();
     } else {
-        // Loop through all the indicies and replace the '_' with a letter.
         for(var i = 0; i < positions.length; i++) {
-            guessingWord[positions[i]] = letter;
+            currentPass[positions[i]] = letter;
         }
     }
 };
 
 function checkWin() {
-    if(guessingWord.indexOf("_") === -1) {
+    if(currentPass.indexOf("_") === -1) {
         document.getElementById("youwin").style.cssText = "display: block";
-        document.getElementById("pressKeyTryAgain").style.cssText= "display: block";
+        document.getElementById("pressKey").style.cssText= "display: block";
         wins++;
         hasFinished = true;
     }
